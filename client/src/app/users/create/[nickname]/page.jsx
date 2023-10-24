48bb2c4a86a90722caf097cceafa5fcb0ce8ed0f
kay0829 <eluka97@daum.net>
2023-10-24 13:04:56 +0900

    [FE] 🐛 빌드 에러 수정 (signle quote - HTML Character Entities)

--- client/src/app/users/create/[nickname]/page.jsx
+++ client/src/app/users/create/[nickname]/page.jsx
@@ -9,12 +9,12 @@ function CreateProfile({ params }) {
             <div className={styles.container}>
                 <div className={styles.col_center}>
                     <div className={styles.main_msg_container}>
-                        <h2 className={styles.main_msg}>You're Almost done!</h2>
+                        <h2 className={styles.main_msg}>You&#39;re Almost done!</h2>
                         <p>
                             You are about to create a new account on <strong>Stack Overflow</strong>
                         </p>
                         <br />
-                        <p>Share a little more about yourself, and we'll show you more relevant questions</p>
+                        <p>Share a little more about yourself, and we&#39;ll show you more relevant questions</p>
                         {/* <p>{nickname}</p> */}
                     </div>
                     <div className={styles.create_form_container}>
